a9d40198ccc7f354545e9f36f184f1fe70fcb329
LLaGeee <brandon_gallman@yahoo.com>
2023-04-06 00:19:14 -0400

    Trial - tested some array logistics

--- MagicSquare.java
+++ MagicSquare.java
@@ -11,5 +11,30 @@ class MagicSquare{
             System.out.print("Enter an odd number from 3 to 11: ");
             size = input.nextInt();
         }while(size % 2 == 0);
+
+        square(size);
+
+        
+
     }
+
+    public static void square(int s){
+        int[][] ms = new int[s][s];
+        int count = 1;
+
+        for(int i = 0; i < s; i++){
+            for(int j = 0; j < s; j++){
+                ms[i][j] = count;
+                count++;
+            }
+        }
+
+        for(int r = 0; r < s; r++){
+            for(int c = 0; c < s; c++){
+                System.out.print(ms[r][c] + "  ");
+            }
+            System.out.println("");
+        }
+    }
+
 }
